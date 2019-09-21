f824f8dc9024d5af9ebfb8490a723cb4e5bbcb5f
Ashley Vidulich <ashleyhelene@gmail.com>
2019-09-21 12:59:06 +1000

    Create basemap

--- BioMap/ui.R
+++ BioMap/ui.R
@@ -8,26 +8,47 @@
 #
 
 library(shiny)
+library(leaflet)
+library(maps)
+library(ggplot2)
+library(ggmap)
+library(maps)
+library(mapdata)
 
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
   
   # Application title
-  titlePanel("Old Faithful Geyser Data"),
+  titlePanel("BioMap Australia"),
   
   # Sidebar with a slider input for number of bins 
-  sidebarLayout(
-    sidebarPanel(
-       sliderInput("bins",
-                   "Number of bins:",
-                   min = 1,
-                   max = 50,
-                   value = 30)
-    ),
-    
-    # Show a plot of the generated distribution
-    mainPanel(
-       plotOutput("distPlot")
-    )
-  )
-))
+
+  tabPanel("Tab 1" ,
+           div(class="outer",
+            sidebarLayout(
+              sidebarPanel(
+                sliderInput("bins",
+                           "Number of bins:",
+                           min = 1,
+                           max = 50,
+                           value = 30)
+              ),
+             
+              # Show a plot of the generated distribution
+              mainPanel(
+                plotOutput("distPlot")
+              )
+            )
+           )
+
+  ),
+  
+  tabPanel("Tab 2" ,
+           #map
+           leafletOutput("mymap",height = 1000)
+
+  ),
+  
+  conditionalPanel("false", icon("crosshair"))
+)
+)

6923eb38b3575064363c6544d9fd9668d7b90911
Tylor Bunting <buntoss@gmail.com>
2019-09-21 13:45:56 +1000

    install load packes fie

---
+++ RScratchPads/TylorBunting_ScratchPad.R
@@ -0,0 +1,6 @@
+
+library(readr)
+
+TB <- list()
+
+TB$Data_1$OutdoorCyclingData <- read_csv("Data/Routes/OutdoorCycle20190921084242_1.csv")
---
+++ install_load_packages.R
@@ -0,0 +1,20 @@
+# source "https://gist.github.com/smithdanielle/9913897"
+# check.packages function: install and load multiple R packages.
+# Check to see if packages are installed. Install them if they are not, then load them into the R session.
+check.packages <- function(pkg){
+  new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
+  if (length(new.pkg)) 
+    install.packages(new.pkg, dependencies = TRUE)
+  sapply(pkg, require, character.only = TRUE)
+}
+
+# set packages to install
+packages<-c("ggplot2", 
+            "rsconnect", 
+            "leaflet", 
+            "ggmap", 
+            "tidyr", 
+            "readr", 
+            "shiny",
+            "mapdata")
+check.packages(packages)
